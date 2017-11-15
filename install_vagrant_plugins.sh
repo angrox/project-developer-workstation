f8fe4cf6420da2c17c87a17324f9d73b7db378f3
Martin Zehetmayer <angrox@idle.at>
2017-11-15 11:02:31 +0100

    Added VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 to installation of vagrant plugins to prevent the too strict check of ffi gem

--- install_vagrant_plugins.sh
+++ install_vagrant_plugins.sh
@@ -1,2 +1,2 @@
-vagrant plugin install vagrant-hostmanager
-vagrant plugin install vagrant-vbguest
+VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 vagrant plugin install vagrant-hostmanager
+VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1 vagrant plugin install vagrant-vbguest
