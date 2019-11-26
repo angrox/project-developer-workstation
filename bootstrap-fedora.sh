682faf99c66b1a848c2e11df2c20dcab1cdb2be1
Martin Zehetmayer <martin.zehetmayer@maibornwolff.de>
2019-11-26 09:54:45 +0100

    Do not copy vagrant help scripts to /usr/local/bin anymore

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 dnf -y install ansible bison flex psmisc libxcrypt-compat
-cp /vagrant/files/bin/* /usr/local/bin
 chmod 755 /usr/local/bin
 ansible-playbook /vagrant/ansible/install_playbook.yaml
