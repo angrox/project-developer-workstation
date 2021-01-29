629bbea0ef3793d99714ebc2710d7b62e9e3c164
Martin Zehetmayer <martin.zehetmayer@maibornwolff.de>
2021-01-29 08:12:38 +0100

    bugfix: bootstrap ansible commands will run as user vagrant

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -2,4 +2,5 @@
 
 dnf -y install ansible bison flex psmisc libxcrypt-compat
 chmod 755 /usr/local/bin
-ansible-playbook /vagrant/ansible/install_playbook.yaml
+su vagrant -c "ansible-galaxy collection install community.general"
+su vagrant -c "ansible-playbook /vagrant/ansible/install_playbook.yaml"
